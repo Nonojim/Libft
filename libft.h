f39ceb898627d6c0ffe5169235192de15525746d
Nonojim <nolan.pederencino@gmail.com>
2025-03-28 01:17:24 +0100

    ft_swap & ft_sort_int_tab added

--- libft.h
+++ libft.h
@@ -33,6 +33,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+void	ft_swap(int *a, int *b);
+void	ft_sort_int_tab(int *tab, int size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
